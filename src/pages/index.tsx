bb41fe2530e6fccfeb7cdbf00e2d4edf7e891d9a
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-06-20 10:42:50 -0600

    Remove AccountDialog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { Header } from "../components/Header";
 import { Layout } from "../components/Layout";
 import { LeftDrawer } from "../components/LeftDrawer";
 import { SceneTable } from "../components/SceneTable";
-import { AccountCredentials, Config } from "../lib/config";
+import { Config } from "../lib/config";
 
 interface Props {
   readonly clientId?: string;
@@ -18,52 +18,21 @@ export function getServerSideProps(): Promise<{ props: Props }> {
   return Promise.resolve({ props: Config });
 }
 
-export default function Home(props: Props): JSX.Element {
-  const [credentials, setCredentials] =
-    React.useState<Partial<AccountCredentials>>(props);
-  // const credentialsValid =
-  //   credentials.clientId && credentials.clientSecret && credentials.vertexEnv;
-  // const [dialogOpen, setDialogOpen] = React.useState(!credentialsValid);
-
-  // function handleAccountClick() {
-  //   setDialogOpen(true);
-  // }
-
-  // function handleConfirm(as: AccountCredentials): void {
-  //   setCredentials(as);
-  //   handleClose();
-  // }
-
-  // function handleClose(): void {
-  //   setDialogOpen(false);
-  // }
-
+export default function Home({ clientId, vertexEnv }: Props): JSX.Element {
   return (
     <Layout
       header={<Header />}
       leftDrawer={<LeftDrawer />}
       leftDrawerOpen
       main={
-        credentials.clientId && credentials.vertexEnv ? (
-          <SceneTable
-            clientId={credentials.clientId}
-            vertexEnv={credentials.vertexEnv}
-          />
+        clientId && vertexEnv ? (
+          <SceneTable clientId={clientId} vertexEnv={vertexEnv} />
         ) : (
           <Paper sx={{ m: 2 }}>
             <Typography>Account credentials required.</Typography>
           </Paper>
         )
       }
-    >
-      {/* {dialogOpen && (
-        <AccountDialog
-          credentials={credentials}
-          onClose={handleClose}
-          onConfirm={handleConfirm}
-          open={dialogOpen}
-        />
-      )} */}
-    </Layout>
+    ></Layout>
   );
 }
