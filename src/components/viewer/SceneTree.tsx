44afff38793610be4a5fe4d5184e84a8079977f5
Jared Dellitt <jared@dellitt.com>
2021-07-01 14:51:16 -0500

    Expand & collapse all

--- src/components/viewer/SceneTree.tsx
+++ src/components/viewer/SceneTree.tsx
@@ -6,36 +6,56 @@ interface Props {
   readonly configEnv: Environment;
   readonly viewerId: string;
   readonly selectedItemdId?: string;
+  readonly expandAll?: boolean;
+  readonly collapseAll?: boolean;
+  readonly onRowClick?: (itemId: string) => void;
 }
 
 export function SceneTree({
   configEnv,
   viewerId,
   selectedItemdId,
+  expandAll,
+  collapseAll,
+  onRowClick,
 }: Props): JSX.Element {
   const ref = React.useRef<HTMLVertexSceneTreeElement>(null);
 
   React.useEffect(() => {
+    const clickRow = async (
+      event: MouseEvent | PointerEvent
+    ): Promise<void> => {
+      const row = await ref?.current?.getRowForEvent(event);
+      if (row?.node == null) return;
+
+      if (onRowClick) onRowClick(row.node.id?.hex || "");
+
+      console.debug(
+        `Selected ${row.node.suppliedId?.value ?? row.node.id?.hex},${
+          row.node.name
+        }`
+      );
+    };
+
     const effectRef = ref.current;
     effectRef?.addEventListener("click", clickRow);
     return () => effectRef?.removeEventListener("click", clickRow);
-  }, [ref]);
+  }, [ref, onRowClick]);
 
   React.useEffect(() => {
     const effectRef = ref.current;
     if (selectedItemdId) effectRef?.scrollToItem(selectedItemdId);
   }, [selectedItemdId]);
 
-  const clickRow = async (event: MouseEvent | PointerEvent): Promise<void> => {
-    const row = await ref?.current?.getRowForEvent(event);
-    if (row?.node == null) return;
+  React.useEffect(() => {
+    const effectRef = ref.current;
+    if (expandAll) effectRef?.expandAll();
+  }, [expandAll]);
 
-    console.debug(
-      `Selected ${row.node.suppliedId?.value ?? row.node.id?.hex},${
-        row.node.name
-      }`
-    );
-  };
+  React.useEffect(() => {
+    const effectRef = ref.current;
+    if (collapseAll) effectRef?.collapseAll();
+  }, [collapseAll]);
 
   return (
     <VertexSceneTree
