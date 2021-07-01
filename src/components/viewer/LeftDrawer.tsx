b4a4592f07351295ceb1cba17a5bd49b2f937c0c
Jared Dellitt <jareddellitt@users.noreply.github.com>
2021-07-01 14:53:27 -0500

    Expand & collapse all (#12)

--- src/components/viewer/LeftDrawer.tsx
+++ src/components/viewer/LeftDrawer.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Drawer, IconButton } from "@material-ui/core";
+import { Box, Divider, Drawer, IconButton, Tooltip } from "@material-ui/core";
 import { drawerClasses } from "@material-ui/core/Drawer";
 import { ChevronLeft } from "@material-ui/icons";
 import { Environment } from "@vertexvis/viewer/dist/types/config/environment";
@@ -15,6 +15,26 @@ interface Props {
   readonly selectedItemdId?: string;
 }
 
+const ExpandAll = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    data-testid="expand-all"
+  >
+    <path d="M14.5,6h-5a.5.5,0,0,0,0,1h5a.5.5,0,0,0,0-1ZM4,2.5H2a.5.5,0,0,0-.46.31.47.47,0,0,0,.11.54l1,1a.48.48,0,0,0,.7,0l1-1a.47.47,0,0,0,.11-.54A.5.5,0,0,0,4,2.5ZM5.5,4h9a.5.5,0,0,0,0-1h-9a.5.5,0,0,0,0,1ZM4,8.5H2a.5.5,0,0,0-.46.31.47.47,0,0,0,.11.54l1,1a.48.48,0,0,0,.7,0l1-1a.47.47,0,0,0,.11-.54A.5.5,0,0,0,4,8.5ZM14.5,9h-9a.5.5,0,0,0,0,1h9a.5.5,0,0,0,0-1Zm0,3h-5a.5.5,0,0,0,0,1h5a.5.5,0,0,0,0-1Z" />
+  </svg>
+);
+
+const CollapseAll = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    data-testid="collapse-all"
+  >
+    <path d="M2.85,8.15A.47.47,0,0,0,2.31,8,.5.5,0,0,0,2,8.5v2a.5.5,0,0,0,.31.46.47.47,0,0,0,.54-.11l1-1a.48.48,0,0,0,0-.7Zm0-6A.47.47,0,0,0,2.31,2,.5.5,0,0,0,2,2.5v2A.5.5,0,0,0,2.31,5a.47.47,0,0,0,.54-.11l1-1a.48.48,0,0,0,0-.7ZM5.5,4h9a.5.5,0,0,0,0-1h-9a.5.5,0,0,0,0,1Zm9,2h-9a.5.5,0,0,0,0,1h9a.5.5,0,0,0,0-1Zm0,3h-9a.5.5,0,0,0,0,1h9a.5.5,0,0,0,0-1Zm0,3h-9a.5.5,0,0,0,0,1h9a.5.5,0,0,0,0-1Z" />
+  </svg>
+);
+
 export function LeftDrawer({
   configEnv,
   onClose,
@@ -22,6 +42,9 @@ export function LeftDrawer({
   viewerId,
   selectedItemdId,
 }: Props): JSX.Element {
+  const [expandAll, setExpandAll] = React.useState<boolean | undefined>();
+  const [collapseAll, setCollapseAll] = React.useState<boolean | undefined>();
+
   return (
     <Drawer
       anchor="left"
@@ -36,10 +59,34 @@ export function LeftDrawer({
         sx={{
           alignItems: "center",
           display: "flex",
-          justifyContent: "flex-end",
+          justifyContent: "space-between",
           py: 1,
         }}
       >
+        <Box>
+          <Tooltip title="Expand All">
+            <IconButton
+              sx={{ width: "36px" }}
+              onClick={() => {
+                setExpandAll(true);
+                setCollapseAll(false);
+              }}
+            >
+              <ExpandAll />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Collapse All">
+          <IconButton
+            sx={{ width: "36px" }}
+            onClick={() => {
+              setCollapseAll(true);
+              setExpandAll(false);
+            }}
+          >
+            <CollapseAll />
+          </IconButton>
+          </Tooltip>
+        </Box>
         <IconButton onClick={onClose}>
           <ChevronLeft />
         </IconButton>
@@ -49,6 +96,12 @@ export function LeftDrawer({
         configEnv={configEnv}
         viewerId={viewerId}
         selectedItemdId={selectedItemdId}
+        expandAll={expandAll}
+        collapseAll={collapseAll}
+        onRowClick={() => {
+          setExpandAll(undefined);
+          setCollapseAll(undefined);
+        }}
       />
     </Drawer>
   );
