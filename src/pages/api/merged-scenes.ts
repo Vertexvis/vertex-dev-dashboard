8597453844ac4aece705e86d8da9d6b7bc7a4809
Madison Ehlers <49169871+MadisonEhlers-Vertex@users.noreply.github.com>
2022-06-29 12:44:08 -0500

    [PLAT-697] Merge scenes with root scene item (#38)
    * [PLAT-697] Merge scenes with root scene item
    
    * remove unused import

--- src/pages/api/merged-scenes.ts
+++ src/pages/api/merged-scenes.ts
@@ -56,7 +56,22 @@ async function create(req: NextIronRequest): Promise<ErrorRes | MergeSceneRes> {
     },
   });
 
+
   const sceneId = s.data.data.id;
+
+ await c.sceneItems.createSceneItem({
+    id: sceneId,
+    createSceneItemRequest: {
+      data: {
+        type: "scene-item",
+        attributes: {
+          name,
+          suppliedId: suppliedId
+        },
+        relationships: {},
+      },
+    },
+  });
 
   const items = await Promise.all(
     sceneIds.map((s) => {
@@ -65,7 +80,9 @@ async function create(req: NextIronRequest): Promise<ErrorRes | MergeSceneRes> {
         createSceneItemRequest: {
           data: {
             type: "scene-item",
-            attributes: {},
+            attributes: {
+              parent: suppliedId
+            },
             relationships: {
               source: {
                 data: {
