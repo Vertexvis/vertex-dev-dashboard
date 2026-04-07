5a76b2cc8ab7154587b9c30cdd1c2075531c18b9
Dan Schultz <schultz.t.dan@gmail.com>
2026-04-07 12:36:48 -0600

    [PLAT-8357] View file metadata (#294)
    * Add file metadata drawer
    
    * Format file metadata as key/value table
    
    * Address PR review feedback
    
    * Tighten metadata table spacing
    
    * Use latest node client lib
    
    * Rename display helpers to formatting

--- src/pages/files.tsx
+++ src/pages/files.tsx
@@ -1,7 +1,9 @@
 import dynamic from "next/dynamic";
 import React from "react";
 
+import { FileDetailsDrawer } from "../components/file/FileDetailsDrawer";
 import { Layout } from "../components/shared/Layout";
+import { File } from "../lib/files";
 import { defaultServerSideProps } from "../lib/with-session";
 
 const FilesTable = dynamic(() => import("../components/file/FileTable"), {
@@ -9,7 +11,24 @@ const FilesTable = dynamic(() => import("../components/file/FileTable"), {
 });
 
 export default function Files(): JSX.Element {
-  return <Layout main={<FilesTable />} />;
+  const [file, setFile] = React.useState<File | undefined>();
+  const drawerOpen = Boolean(file);
+
+  return (
+    <Layout
+      main={
+        <FilesTable activeFileId={file?.id} onFileSelected={setFile} />
+      }
+      rightDrawer={
+        <FileDetailsDrawer
+          file={file}
+          onClose={() => setFile(undefined)}
+          open={drawerOpen}
+        />
+      }
+      rightDrawerOpen={drawerOpen}
+    />
+  );
 }
 
 export const getServerSideProps = defaultServerSideProps;
