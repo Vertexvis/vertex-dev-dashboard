01c0cd92949f35bd0cc0607bea4681650ae0d147
Madison Ehlers <madison.ehlers@vertex3d.com>
2026-06-15 13:10:29 -0400

    [PLAT-7099] Report Jest coverage to Sonar

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.test.{ts,tsx}",
+    "!src/pages/_app.tsx",
+    "!src/pages/_document.tsx",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
   preset: "ts-jest",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testEnvironment: "jsdom",
