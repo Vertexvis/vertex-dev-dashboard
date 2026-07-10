ca4a49ba877aa83d99e3494d0db68283bc48d430
Madison Ehlers <madison.ehlers@vertex3d.com>
2026-07-10 10:45:48 -0500

    Fix MSW test helper build compatibility

--- test/msw/install.ts
+++ test/msw/install.ts
@@ -1,33 +1,80 @@
-import nodeFetch, { Headers, Request, Response } from "node-fetch";
-
 import { server } from "./server";
 
-const fetchImplementation = ((
-  input: RequestInfo | URL,
-  init?: RequestInit
-): Promise<Response> => {
-  if (typeof input === "string" || input instanceof URL) {
-    return nodeFetch(new URL(input.toString(), "http://localhost").toString(), init);
-  }
+interface FetchApis {
+  readonly fetch: typeof fetch;
+  readonly Headers: typeof Headers;
+  readonly Request: typeof Request;
+  readonly Response: typeof Response;
+}
 
-  return nodeFetch(input as never, init);
-}) as unknown as typeof fetch;
+function createRelativeUrlFetch(nativeFetch: typeof fetch): typeof fetch {
+  return ((
+    input: RequestInfo | URL,
+    init?: RequestInit,
+  ): Promise<globalThis.Response> => {
+    if (typeof input === "string" || input instanceof URL) {
+      return nativeFetch(
+        new URL(input.toString(), "http://localhost").toString(),
+        init,
+      );
+    }
 
-export function installMockServer(): void {
-  beforeAll(() => {
-    Object.assign(global, {
+    return nativeFetch(input, init);
+  }) as typeof fetch;
+}
+
+function getFetchApis(): FetchApis {
+  const { fetch: nativeFetch, Headers, Request, Response } = globalThis;
+
+  if (
+    nativeFetch != null &&
+    Headers != null &&
+    Request != null &&
+    Response != null
+  ) {
+    return {
+      fetch: nativeFetch,
       Headers,
       Request,
       Response,
-      fetch: fetchImplementation,
+    };
+  }
+
+  // Jest's jsdom environment may not expose fetch globals even though Node can.
+  const nodeFetchModule = require("node-fetch") as {
+    readonly default: typeof fetch;
+    readonly Headers: typeof Headers;
+    readonly Request: typeof Request;
+    readonly Response: typeof Response;
+  };
+
+  return {
+    fetch: nodeFetchModule.default,
+    Headers: nodeFetchModule.Headers,
+    Request: nodeFetchModule.Request,
+    Response: nodeFetchModule.Response,
+  };
+}
+
+export function installMockServer(): void {
+  beforeAll(() => {
+    const apis = getFetchApis();
+
+    Object.assign(global, {
+      Headers: apis.Headers,
+      Request: apis.Request,
+      Response: apis.Response,
+      fetch: createRelativeUrlFetch(apis.fetch),
     });
 
     server.listen({ onUnhandledRequest: "error" });
   });
 
   afterEach(() => {
+    const apis = getFetchApis();
+
     server.resetHandlers();
-    Object.assign(global, { fetch: fetchImplementation });
+    Object.assign(global, { fetch: createRelativeUrlFetch(apis.fetch) });
   });
 
   afterAll(() => {
